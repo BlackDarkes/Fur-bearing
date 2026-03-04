697ce16934fbae725488e00d243422155e74f9af
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-03-04 15:14:06 +0500

    Попарвлен workflow

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: "export",
-  // basePath: "/Fur-bearing",
+  basePath: "/Fur-bearing",
   images: {
     unoptimized: true,
   }
