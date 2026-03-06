ccafdc8043f52316f890529cd614d8cd636cc2d2
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-03-06 20:27:17 +0500

    Полностью сделаный footer

--- src/components/footer/ui/Footer.tsx
+++ src/components/footer/ui/Footer.tsx
@@ -6,10 +6,10 @@ import { FooterSecure } from "./footer-secure/FooterSecure";
 export const Footer = () => {
   return (
     <footer className="mt-footer-margin-top pt-[clamp(60px,20vw,100px)] border-t border-opacity-bg">
-      <Container className="flex justify-between items-start pb-[clamp(80px,20vw,140px)]">
+      <Container className="flex justify-between items-start pb-[clamp(80px,20vw,140px)] max-lg:flex-col max-lg:gap-y-[clamp(40px,4vw,60px)] max-lg:w-fit">
         <Logo />
 
-        <div className="flex gap-x-[clamp(40px,10vw,120px)]">
+        <div className="flex gap-x-[clamp(40px,4vw,120px)] max-md:flex-col max-md:gap-y-[clamp(60px,4vw,80px)]">
           <FooterListLink items={FOOTER_LINKS_ITEMS} />
           <FooterListLink items={FOOTER_SECURE_ITEMS} />
         </div>
--- src/components/footer/ui/footer-list/FooterListLinkItem.tsx
+++ src/components/footer/ui/footer-list/FooterListLinkItem.tsx
@@ -1,4 +1,5 @@
 import { IFooterLinksItems } from "@/constants/footer/footer-links-items";
+import { cn } from "@/shared/lib/utils";
 import Link from "next/link";
 
 interface IFooterListLinkItemProps {
@@ -11,7 +12,13 @@ export const FooterListLinkItem = ({ item }: IFooterListLinkItemProps) => {
       {item.isTitle ? (
         <h3 className="mb-5 text-[clamp(18px,8vw,20px)]">{item.message}</h3>
       ) : (
-        <Link href={item.link || "#"} className="block mb-2 text-[clamp(14px,8vw,16px)]">
+        <Link
+          href={item.link || "#"}
+          className={cn(`relative block mb-2 text-[clamp(12px,4vw,16px)] w-fit transition duration-400
+          before:content-[""] before:absolute before:-bottom-0.75 before:left-0 before:w-full before:h-0.5 before:bg-accent-color before:transition-all before:duration-400 before:scale-0 before:origin-left
+          hover:text-accent-color hover:before:scale-100
+          active:opacity-80 before:active:opacity-80`)}
+        >
           <b>{item.name}</b> {item.message}
         </Link>
       )}
